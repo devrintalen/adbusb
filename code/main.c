380dc4fa6ffd988cade6cf8c69d7f2566f2e110e
Devrin <devrin.talen@gmail.com>
2009-10-25 09:06:15 -0400

    Initializing some code files.

---
+++ code/main.c
@@ -0,0 +1,29 @@
+// Copyright 2009 Devrin Talen
+// This file is part of ADBUSB.
+// 
+// ADBUSB is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// ADBUSB is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with ADBUSB.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "adb.h"
+
+uint8_t main(void)
+{
+    while(1) ;
+
+    return 0;
+}
